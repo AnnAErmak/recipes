8fae1c160a231663146f23ac6a05cb38654d039e
Ermak Anna <ermak-anna86@yandex.ru>
2024-04-16 09:22:39 +0300

    init project

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import styles from './App.module.scss'
+import Button from "../components/Button";
+
+const App = () => {
+    null
+}
+
+export default App
---
+++ src/App/pages/Recipes/Recipes.tsx
@@ -0,0 +1,3 @@
+const Recipes = () => null
+
+export default Recipes
